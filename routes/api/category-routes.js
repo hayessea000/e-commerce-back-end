81b9edcabb582be60d728e6f68e67cf83c79882b
Sean Hayes <hayessea000@outlook.com>
2023-05-09 18:13:28 -0500

    finished product route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,6 +10,10 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product}]
     });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
