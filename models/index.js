c13da60ccdefac41ba248669ce9825bbe8d20708
Sean Hayes <hayessea000@outlook.com>
2023-05-09 16:04:20 -0500

    added relation in index model

--- models/index.js
+++ models/index.js
@@ -6,16 +6,29 @@ const Tag = require('./Tag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  // foreignKey: '_id',
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  // foreignKey: '_id',
-  // onDelete: 'CASCADE',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tags'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_products'
+});
 
 module.exports = {
   Product,
